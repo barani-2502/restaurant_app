314cd6944007c4d48fbe0a6322420140962d37ce
barani-2502 <barani@testpress.in>
2025-10-16 14:49:51 +0530

    feat: Register and customize admin dashboard for restaurants app (#3)
    * feat: Register and customize admin dashboard for restaurants app
    
    Registered all models, customized list_display, search_fields, ordering, and inlines for photos.
    
    * chore: Set extra=0 for photo inlines to reduce empty forms

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Restaurant, MenuItem, MenuItemPhoto, RestaurantPhoto, Cuisine
 
-# Register your models here.
+class MenuItemPhotoInline(admin.TabularInline):
+    model = MenuItemPhoto
+    extra = 0 
+
+class RestaurantPhotoInline(admin.TabularInline):
+    model = RestaurantPhoto
+    extra = 0
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'open_status', 'spotlight')
+    search_fields = ('name', 'address', 'city')
+    ordering = ('name',)
+    inlines = [RestaurantPhotoInline]
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'restaurant', 'price')
+    list_editable = ('price',) 
+    search_fields = ('name',)
+    list_filter = ('restaurant',)
+    ordering = ('restaurant', 'name')
+    inlines = [MenuItemPhotoInline]
+
+@admin.register(MenuItemPhoto)
+class MenuItemPhotoAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'image')
+
+@admin.register(RestaurantPhoto)
+class RestaurantPhotoAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'image')
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
