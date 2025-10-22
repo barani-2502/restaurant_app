9c2d31f8df24d3e01c98360f07e24e279c7ca6dd
barani-2502 <barani@testpress.in>
2025-10-22 13:11:11 +0530

    feat: Implement homepage with spotlight section (#6)
    * feat: Implement homepage with spotlight section
    
     - Displayed spotlighted restaurants from the database
     - Included login and register buttons
     - Added browser restaurants button to see all restaurants
    
    * perf: Optimize spotlight logic
    
     - Used prefetch_related to eliminate N+1 query issue for restaurant_photos
     - Replaced magic number with SPOTLIGHT_RESTAURANT_COUNT constant for clarity and maintainability
    
    * refactor: Convert home view to class based TemplateView
    
    * refactor: Extract restaurant card into a reusable template
    
    * refactor: Convert to ListView and added pagination
    
    * refactor: Set pagination count and remove constant

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Restaurant
 
-# Create your views here.
+
+class HomePageView(ListView):
+    model = Restaurant
+    template_name = 'restaurants/home.html'
+    context_object_name = 'spotlighted_restaurants'
+    paginate_by = 9
+
+    def get_queryset(self):
+        return Restaurant.objects.filter(spotlight=True).prefetch_related('restaurant_photos')
+
