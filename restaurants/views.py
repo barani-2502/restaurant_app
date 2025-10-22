4dc9fcd1abaaf77f34994591c7a46fc417dc24b9
barani-2502 <barani@testpress.in>
2025-10-22 12:13:50 +0530

    perf: Optimize spotlight logic
     - Used prefetch_related to eliminate N+1 query issue for restaurant_photos
     - Replaced magic number with SPOTLIGHT_RESTAURANT_COUNT constant for clarity and maintainability

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Restaurant
 
+SPOTLIGHT_RESTAURANT_COUNT = 5
+
 def home(request):
-    spotlighted_restaurants = Restaurant.objects.filter(spotlight=True)[:5]
+    spotlighted_restaurants = Restaurant.objects.filter(spotlight=True)[:5].prefetch_related('restaurant_photos')[:SPOTLIGHT_RESTAURANT_COUNT]
     context = {
         'spotlighted_restaurants': spotlighted_restaurants
     }
