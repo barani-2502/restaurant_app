d71dc8501bde49be3d61d6fbc5521bc5bdea5c60
barani-2502 <barani@testpress.in>
2025-10-22 17:42:21 +0530

    feat: Add Restaurant ListView and DetailView with templates and URL routes
    Implemented class-based views for list and detail views of restaurants. Added pagination support in the list view. Created new templates -  restaurant_list.html and restaurant_detail.html. Updated urls.py to include paths for list and detail views.

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, RestaurantListView, RestaurantDetailView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
+    path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
 ]
