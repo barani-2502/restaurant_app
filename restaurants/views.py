d45740844a2b257eade554f4f739ab4e8466fde9
barani-2502 <barani@testpress.in>
2025-10-22 11:21:36 +0530

    feat: Implement homepage with spotlight section
     - Displayed spotlighted restaurants from the database
     - Included login and register buttons
     - Added browser restaurants button to see all restaurants

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Restaurant
 
-# Create your views here.
+def home(request):
+    spotlighted_restaurants = Restaurant.objects.filter(spotlight=True)[:5]
+    context = {
+        'spotlighted_restaurants': spotlighted_restaurants
+    }
+
+    return render(request, 'restaurants/home.html', context)
+
