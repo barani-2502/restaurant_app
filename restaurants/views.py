e8737f2d8bbd9d729cffaa9a7a754d377b836a8c
barani-2502 <barani@testpress.in>
2025-10-22 13:08:28 +0530

    refactor: Set pagination count and remove constant

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,13 +1,12 @@
 from django.views.generic import ListView
 from .models import Restaurant
 
-SPOTLIGHT_RESTAURANT_COUNT = 6
 
 class HomePageView(ListView):
     model = Restaurant
     template_name = 'restaurants/home.html'
     context_object_name = 'spotlighted_restaurants'
-    paginate_by = SPOTLIGHT_RESTAURANT_COUNT
+    paginate_by = 9
 
     def get_queryset(self):
         return Restaurant.objects.filter(spotlight=True).prefetch_related('restaurant_photos')
