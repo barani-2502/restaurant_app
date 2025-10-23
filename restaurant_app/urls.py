79ac3fead8ef4844b6824c4684cbbf7a0d40a391
barani-2502 <barani@testpress.in>
2025-10-23 17:58:13 +0530

    feat: Add login and logout templates, URL routing, and base authentication template
    - Added authentication URLs in urls.py
    - Extracted repeated code into a reusable file base_auth.html with proper blocks and structure
    - Updated styles.css for input field visibility in login form UI

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('restaurants.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
 
