acc615802003cde6db55ca77f019801054685612
barani-2502 <barani@testpress.in>
2025-10-23 17:45:00 +0530

    chore: Update settings.py for redirect url and base template for tailwind cdn

--- restaurant_app/settings.py
+++ restaurant_app/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
@@ -130,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
