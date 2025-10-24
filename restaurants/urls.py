b5e06158b75fc5a1f018d136b9f1c5f3848eec88
barani-2502 <barani@testpress.in>
2025-10-24 11:01:26 +0530

    Revert commit 0474494: remove restaurant detail and list pages

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import HomePageView, RestaurantListView, RestaurantDetailView
+from .views import HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
-    path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    path('', HomePageView.as_view(), name='home')
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .models import Restaurant
 
 
@@ -6,23 +6,8 @@ class HomePageView(ListView):
     model = Restaurant
     template_name = 'restaurants/home.html'
     context_object_name = 'spotlighted_restaurants'
-    paginate_by = 6
+    paginate_by = 9
 
     def get_queryset(self):
         return Restaurant.objects.filter(spotlight=True).prefetch_related('restaurant_photos')
 
-class RestaurantListView(ListView):
-    model = Restaurant
-    template_name = 'restaurants/restaurant_list.html'
-    context_object_name = 'restaurants'
-    paginate_by = 6
-
-class RestaurantDetailView(DetailView):
-    model = Restaurant
-    template_name = 'restaurants/restaurant_detail.html'
-    content_object_name = 'restaurants'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['menu_items'] = self.object.menu_items.all()
-        return context
