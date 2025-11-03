5d370975774d9650a18860d7a771ce1616b67d35
barani-2502 <barani@testpress.in>
2025-11-03 18:23:49 +0530

    feat: Add user bookmark page with URL routing, templates and testcases (#28)
    * feat: Add user bookmark page with URL routing, templates and testcases
    
    Add class-based views for managing user bookmarks. Add bookmarks to Django admin. Added link to access user bookmarks page from navbar. Includes initial test cases for bookmark list
    
    * chore: Rename template file

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, RestaurantListView, RestaurantDetailView, RegisterView, UserProfileView, UserProfileEditView
+from .views import HomePageView, RestaurantListView, RestaurantDetailView, RegisterView, UserProfileView, UserProfileEditView, UserBookmarksListView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('profile/edit/', UserProfileEditView.as_view(), name='profile_edit'),
+    path('bookmarks/', UserBookmarksListView.as_view(), name='bookmarks_list'),
 ]
