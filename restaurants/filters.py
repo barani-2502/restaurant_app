9a681ae7f2761ec4ad85985d71fe1d6c2149dce6
barani-2502 <barani@testpress.in>
2025-11-07 16:24:24 +0530

    refactor: Update food type filter as ChoiceFilter instead of CharFilter

--- restaurants/filters.py
+++ restaurants/filters.py
@@ -5,7 +5,12 @@ class RestaurantFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains', label='Name')
     city = django_filters.CharFilter(field_name='city', lookup_expr='icontains', label='City')
     cuisines = django_filters.CharFilter(field_name='cuisines__name', lookup_expr='icontains', label='Cuisine')
-    food_type = django_filters.CharFilter(field_name='food_type', lookup_expr='iexact', label='Food type')
+    food_type = django_filters.ChoiceFilter(
+        choices=[('', 'Select Food Type')] + Restaurant.FoodType.choices, 
+        field_name='food_type',
+        label='',
+        empty_label=None,
+    )
     open_status = django_filters.BooleanFilter(field_name='open_status')
 
     class Meta:
